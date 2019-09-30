5bd3784f179b2133300fa1c7ec0aa0ef8fc3e144
Rustyn Simmons <Rustyn@rustynsimmons.com>
2019-09-30 13:06:44 -0700

    Add seed data into user table when program is run.

---
+++ Persistence/Seed.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Identity;
+
+namespace Persistence
+{
+    public class Seed
+    {
+        public static async Task SeedData(StockAppContext context, UserManager<AppUser> userManager)
+        {
+            if (!userManager.Users.Any())
+            {
+                var users = new List<AppUser>
+                {
+                    new AppUser
+                    {
+                        DisplayName = "Rustyn",
+                        UserName = "rmckays",
+                        Email = "rustyn@email.com"
+                    },
+                    new AppUser
+                    {
+                        DisplayName = "Andy",
+                        UserName = "andys",
+                        Email = "andy@email.com"
+                    },
+                    new AppUser
+                    {
+                        DisplayName = "Nataly",
+                        UserName = "natalyvits",
+                        Email = "nataly@email.com"
+                    }
+                };
+                foreach (var user in users)
+                {
+                    await userManager.CreateAsync(user, "P@$$w0rd");
+                }
+            }
+        }
+    }
+}
