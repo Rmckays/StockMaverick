08d593626bdb0a056d04c4bf73c93117ec969ce3
Rustyn Simmons <Rustyn@rustynsimmons.com>
2019-10-13 17:28:14 -0700

    Add google chart component.

--- API/client/src/Components/Cards/GraphCard.tsx
+++ API/client/src/Components/Cards/GraphCard.tsx
@@ -1,5 +1,61 @@
 import React from 'react';
+import {Chart} from "react-google-charts";
+
+import style from '../Components.module.css'
+import {Card} from "semantic-ui-react";
 
 
 
-export {};
+
+const GraphCard = () => {
+    return (
+        <Card raised className={style.graphCard}>
+            <Chart
+                width={'600px'}
+                height={'400px'}
+                chartType="LineChart"
+                loader={<div>Loading Chart</div>}
+                data={[
+                    [
+                        { type: 'date', label: 'Day' },
+                        'Closing Price',
+                    ],
+                    [new Date(2019, 10, 1), 5.7],
+                    [new Date(2019, 10, 2), 8.7],
+                    [new Date(2019, 10, 3), 12],
+                    [new Date(2019, 10, 4), 15.3],
+                    [new Date(2019, 10, 5), 18.6],
+                    [new Date(2019, 10, 6), 20.9],
+                    [new Date(2019, 10, 7), 19.8],
+                    [new Date(2019, 10, 8), 16.6],
+                    [new Date(2019, 10, 9), 13.3],
+                    [new Date(2019, 10, 10), 9.9],
+                    [new Date(2019, 10, 11), 6.6],
+                    [new Date(2019, 10, 12), 4.5],
+                ]}
+                options={{
+                    chart: {
+                        title:
+                            'Market Price of AAPL',
+                    },
+                    width: 600,
+                    height: 400,
+                    series: {
+                        // Gives each series an axis name that matches the Y-axis below.
+                        0: { axis: 'Price' },
+                    },
+                    axes: {
+                        // Adds labels to each axis; they don't have to match the axis names.
+                        y: {
+                            Price: { label: 'Price in USD$' },
+
+                        },
+                    },
+                }}
+                rootProps={{ 'data-testid': '4' }}
+            />
+        </Card>
+    );
+};
+
+export default GraphCard;
--- API/client/src/Containers/CommandCenter.tsx
+++ API/client/src/Containers/CommandCenter.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 import Login from "../Components/Login/Login";
 import style from './Containers.module.css';
+import {Card} from "semantic-ui-react";
+import GraphCard from "../Components/Cards/GraphCard";
 
 const CommandCenter = () => {
     return (
         <div className={style.commandCenter}>
+            <GraphCard/>
             <Login/>
         </div>
     );
