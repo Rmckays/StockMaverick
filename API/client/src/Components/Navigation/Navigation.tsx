b38f7fc599a73bb5dead8c11a808d1a031a17d02
Rustyn Simmons <rustyn@rustynsimmons.com>
2019-11-01 22:32:58 -0700

    Add additional styling for Menu.Items

--- API/client/src/Components/Navigation/Navigation.tsx
+++ API/client/src/Components/Navigation/Navigation.tsx
@@ -17,7 +17,7 @@ const Navigation: React.FC<IProps> = () => {
             <Menu vertical className={style.navigation}>
                 <Image className={style.profileImage} src='https://react.semantic-ui.com/images/wireframe/square-image.png' size='small' circular />
                 <Menu.Item as={NavLink} exact className={style.navLinks} to='/dashboard'> Dashboard </Menu.Item>
-                <Menu.Item as={NavLink} className={style.navLinks} to='/portfolio'> Portfolio </Menu.Item>
+                <Menu.Item as={NavLink} exact className={style.navLinks} to='/portfolio'> Portfolio </Menu.Item>
                 <NavLink className={style.navLinks} to='/wallet'> Wallet </NavLink>
                 <NavLink className={style.navLinks} to='/profile'> Profile </NavLink>
                 <NavLink className={style.navLinksBottom} to='/about'> About </NavLink>
